cecb213d15cbc50ea60acf40cbb83af345aaa6ca
Ben <ben@armosec.io>
2023-04-25 11:24:29 +0300

    Defining the main interface

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # kubescape-network-scanner
 Network scan and service discovery package
+
+## Basic network package
+
+See the network scanner and service discovery package and service discovery [here](internal/pkg/network-scanner/).
+
+
+
+
---
+++ internal/pkg/network-scanner/interface.go
@@ -0,0 +1,49 @@
+package networkscanner
+
+import "net"
+
+////////////////////////////////////////////////////////////////////////////////////////
+// Interface definition for network scanner and service discovery
+////////////////////////////////////////////////////////////////////////////////////////
+
+const (
+	TARGET_TYPE_IP                                = "IP"
+	TARGET_TYPE_IP_LIST                           = "IP_LIST"
+	TARGET_TYPE_IP_RANGE                          = "IP_RANGE"
+	TARGET_TYPE_HOSTNAME                          = "HOSTNAME"
+	PORT_TYPE_SINGLE                              = "SINGLE"
+	PORT_TYPE_LIST                                = "LIST"
+	PORT_TYPE_RANGE                               = "RANGE"
+	AUTHENTICATION_STATUS_AUTHENTICATED           = "AUTHENTICATED"
+	AUTHENTICATION_STATUS_UNAUTHENTICATED         = "UNAUTHENTICATED"
+	AUTHENTICATION_STATUS_PARTIALLY_AUTHENTICATED = "PARTIALLY_AUTHENTICATED"
+)
+
+// Struct defining targets of the network scanner
+type TargetDescription struct {
+	TargetType string
+	IPs        []net.IP
+	IPStart    net.IP
+	IPEnd      net.IP
+	Hostname   string
+	PortType   string
+	Ports      []int
+	PortStart  int
+	PortEnd    int
+	TcpPorts   bool
+	UdpPorts   bool
+}
+
+// Struct defining the result of the network scanner
+type ScanResult struct {
+	IP             net.IP
+	Port           int
+	Service        string // Maybe we need to break this down into more fields (HTTP, Kubelete etc.)
+	Authenticated  string
+	SecureProtocol bool // TLS/SSL
+}
+
+// Interface for network scanner
+type NetworkScanner interface {
+	Scan(target TargetDescription) ([]ScanResult, error)
+}
